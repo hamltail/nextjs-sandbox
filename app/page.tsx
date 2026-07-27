b49aa6c1abee00084840f5c853e07071e71bd876
h-waji <goo.hiro.w@gmail.com>
2026-07-27 15:25:36 +0900

    Heroセクションを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Container from "@/components/Container";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <section className="px-7 py-12 md:px-11 xl:px-0">
-      <Container>
-        <h1 className="font-en text-4xl font-bold">
-          Next.js Sandbox
-        </h1>
-
-        <p className="mt-2">
-          Next.jsを学ぶための検証用アプリです。
-        </p>
-      </Container>
-    </section>
-  );
+  return <Hero />;
 }
