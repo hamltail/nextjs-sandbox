5b877ecc397f2e8528a20eefa74182ca42d064be
h-waji <goo.hiro.w@gmail.com>
2026-07-28 09:56:43 +0900

    ユーザー登録ページを追加

---
+++ app/signup/page.tsx
@@ -0,0 +1,17 @@
+import Container from "@/components/Container";
+
+export default function SignupPage() {
+  return (
+    <section className="px-7 py-16 md:px-11 xl:px-0">
+      <Container>
+        <h1 className="font-en text-4xl font-bold">
+          Sign up
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          This will be a signup page for new users.
+        </p>
+      </Container>
+    </section>
+  );
+}
