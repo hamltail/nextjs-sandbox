02120f01d8b86aa876422930b1527111011351a9
h-waji <goo.hiro.w@gmail.com>
2026-08-27 18:14:28 +0900

    StorybookのVitest連携を削除

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,7 @@
 import path from "node:path";
-import { defineConfig } from "vitest/config";
-import { fileURLToPath } from "node:url";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import { playwright } from "@vitest/browser-playwright";
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+import { defineConfig } from "vitest/config";
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -16,37 +9,7 @@ export default defineConfig({
     },
   },
   test: {
-    projects: [
-      {
-        extends: true,
-        test: {
-          include: ["**/*.test.ts"],
-          exclude: ["**/node_modules/**", "**/tests/**"],
-        },
-      },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-        },
-      },
-    ],
+    include: ["**/*.test.ts"],
+    exclude: ["**/node_modules/**", "**/tests/**"],
   },
 });
