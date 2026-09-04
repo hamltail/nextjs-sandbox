41f0dc9005561643e5194c9380158cf59095df7e
h-waji <goo.hiro.w@gmail.com>
2026-09-04 11:05:07 +0900

    Storybookで翻訳を利用できるように修正

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,24 @@
 import "../app/globals.css";
 
 import type { Preview } from "@storybook/nextjs-vite";
+import { NextIntlClientProvider } from "next-intl";
+
+const messages = {
+  ConfirmModal: {
+    cancel: "キャンセル",
+    delete: "削除",
+  },
+};
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider locale="ja" messages={messages}>
+        <Story />
+      </NextIntlClientProvider>
+    ),
+  ],
+
   parameters: {
     controls: {
       matchers: {
